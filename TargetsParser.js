d5365afe8e749e4fda7a799916a530077c577fb6
DannyClink <imerator.gun@gmail.com>
2022-04-12 21:35:43 +0300

    update Targets parser

--- TargetsParser.js
+++ TargetsParser.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const { exec } = require("child_process");
 
 const messageContainer = require("./messageContainer");
 
@@ -6,14 +7,18 @@ class TargetsParser {
   #multiplePorts = new RegExp(/([0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3} \(([0-9]{1,3}\/tcp, [0-9]{1,3}\/tcp)\))/, "g");
   #onePort = new RegExp(/([0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3} \(([0-9]{1,3}\/tcp))\)/, "g");
 
-  generateTargets() {
+  async generateTargets() {
     const targetsMultiplePorts = this._handleTargets(this.#multiplePorts);
     const targetsOnePort = this._handleTargets(this.#onePort);
 
-    fs.writeFileSync(
+    await fs.writeFileSync(
       "./targets.json",
       JSON.stringify({ data: [...targetsMultiplePorts, ...targetsOnePort] }, null, 2)
     );
+
+    exec(`git add targets.json"`, this._handleConsoleCommands.bind(this));
+    exec(`git commit -m "update targets"`, this._handleConsoleCommands.bind(this));
+    exec(`git push`, this._handleConsoleCommands.bind(this));
   }
 
   _handleTargets(regexp) {
@@ -31,6 +36,12 @@ class TargetsParser {
 
         return result; 
     }, []);
+  }
+
+  _handleConsoleCommands(error, stdout, stderr) {
+    if(error) { console.log(error); }
+    if(stdout) { console.log(); }
+    if(stderr) { console.log(); }
   }
 }
 
